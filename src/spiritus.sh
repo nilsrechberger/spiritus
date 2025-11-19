b22134676829521515388887d60fa4a44b6c849f
Nils Rechberger <nilsrechberger@outlook.com>
2025-11-19 10:25:48 +0100

    feat: convert JSON response to CSV table

--- src/spiritus.sh
+++ src/spiritus.sh
@@ -6,4 +6,4 @@ source ./.env
 # Request data
 curl --request GET \
 --url "https://api.openaq.org/v3/locations/8118" \
---header "X-API-Key: $API_KEY" | jq "."
+--header "X-API-Key: $API_KEY" | jq -r '.results[] | [.id, .name, .location, .country_name] | @csv'
