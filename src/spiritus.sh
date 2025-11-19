26715a3c48e5a89b1b22dedac095cd760e0947f1
Nils Rechberger <nilsrechberger@outlook.com>
2025-11-19 11:07:19 +0100

    feat: request sensor values

--- src/spiritus.sh
+++ src/spiritus.sh
@@ -5,5 +5,5 @@ source ./.env
 
 # Request data
 curl --request GET \
---url "https://api.openaq.org/v3/locations/8118" \
---header "X-API-Key: $API_KEY" | jq -r '.results[] | [.id, .name, .location, .country_name] | @csv'
+--url "https://api.openaq.org/v3/sensors/3917/days" \
+--header "X-API-Key: $API_KEY" | jq -r '.results[] | [.value, .parameter.name, .parameter.units, .period.datetimeFrom.local] | @csv' | column -ts ","
