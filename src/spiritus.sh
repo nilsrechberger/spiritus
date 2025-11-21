b9bf00bbc40324525a76e0523b35a11a6920707b
Nils Rechberger <nilsrechberger@outlook.com>
2025-11-21 11:35:17 +0100

    feat: merge data to single CSV

--- src/spiritus.sh
+++ src/spiritus.sh
@@ -3,12 +3,11 @@
 shopt -s globstar
 
 # Remove old data
-rm -r data/
+rm -fr data/
 
 # Define scope
 years=(
 	2024
-	2025
 )
 
 # Request data from AWS and download
@@ -30,3 +29,31 @@ for file in data/**/*.csv.gz; do
 	echo "Unzip $file"
 done
 echo "Done unzip data"
+
+
+# Merge data
+MERGED_DATA="data/merged_data.csv"
+
+# Remove old data
+rm -f $MERGED_DATA
+touch $MERGED_DATA
+
+# Extraxt header
+FIRST_FILE=""
+for file in data/**/*.csv; do
+    if [ -f "$file" ]; then
+        FIRST_FILE="$file"
+        break  # Stoppe nach der ersten gefundenen Datei
+    fi
+done
+
+head -n 1 "$FIRST_FILE" > "$MERGED_DATA"
+
+for file in data/**/*.csv; do
+    if [ -f "$file" ]; then
+        tail -n +2 "$file" >> "$MERGED_DATA"
+    fi
+done
+
+echo "Data merged"
+echo "Output: $MERGED_DATA"
