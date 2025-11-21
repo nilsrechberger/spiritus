0ddcc0b97d5d761f8681c84e9ca02614395807ae
Nils Rechberger <nilsrechberger@outlook.com>
2025-11-21 08:23:28 +0100

    feat: unzip data

--- src/spiritus.sh
+++ src/spiritus.sh
@@ -1,13 +1,28 @@
 #!/bin/bash
 
+shopt -s globstar
+
+# Define scope
 years=(
 	2024
 )
 
+# Request data from AWS and download
+echo "Start Download..."
 for year in "${years[@]}"; do
 	aws s3 cp \
 	--no-sign-request \
 	--recursive \
 	s3://openaq-data-archive/records/csv.gz/locationid=2178/year=$year/ \
 	data
+	echo "Download data for year $year."
 done
+echo "Finished download."
+
+# Unzip data
+echo "Start unzip data..."
+for file in data/**/*.csv.gz; do
+	gunzip $file
+	echo "Unzip $file"
+done
+echo "Done unzip data"
