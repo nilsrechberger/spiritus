f369e5f2985a9c511f583728a7cbad1b5408a991
Nils Rechberger <nilsrechberger@outlook.com>
2025-11-21 08:04:06 +0100

    feat: define data scope by years

--- src/spiritus.sh
+++ src/spiritus.sh
@@ -1,35 +1,13 @@
 #!/bin/bash
 
-# Define request urls
-requests_urls=(
-    "https://openaq-data-archive.s3.amazonaws.com/records/csv.gz/locationid=2178/year=2022/month=05/location-2178-20220503.csv.gz"
+years=(
+	2024
 )
 
-# Check if data dir exists
-data_dir="./data"
-
-if [ ! -d "$data_dir" ]; then
-    echo "Creating dir $data_dir..."
-    mkdir -p "$data_dir"
-fi
-
-# Request and download data
-echo "Start download..."
-for url in "${requests_urls[@]}"; do
-    echo "Downloade: $url"
-    wget -q -P "$data_dir" "$url"
+for year in "${years[@]}"; do
+	aws s3 cp \
+	--no-sign-request \
+	--recursive \
+	s3://openaq-data-archive/records/csv.gz/locationid=2178/year=$year/ \
+	data
 done
-echo "Download finished."
-
-# Unpack data
-echo "Unpack .gz-Files..."
-for file in "$data_dir"/*.csv.gz; do
-    if [ -f "$file" ]; then
-        echo "Unapck: $file"
-        gunzip "$file"
-    else
-        echo "No .csv.gz-Files in $data_dir found."
-        break
-    fi
-done
-echo "Unpack finished."
