02846cf18c235f27c4855d36109a1a5ac199236f
Nils Rechberger <nilsrechberger@outlook.com>
2025-11-25 16:23:47 +0100

    feat: iterate over location IDs

--- src/processor.sh
+++ src/processor.sh
@@ -4,8 +4,8 @@
 
 MERGED_DATA="data/merged_data.csv"
 
-# Filter columns for parameter
-
+# Filter data for parameter
+echo "Start filter data for paramters"
 PARAMETERS=(
 	co
 	no2
@@ -16,7 +16,26 @@ PARAMETERS=(
 )
 
 for param in "${PARAMETERS[@]}"; do
+	echo "Filter for $param"
 	OUTPUT="data/$param.csv"
 	head -n 1 "$MERGED_DATA" > "$OUTPUT"
-	grep -e "$param" $MERGED_DATA >> "$OUTPUT"
+	grep -e "$param" "$MERGED_DATA" >> "$OUTPUT"
+	echo "Output file: $OUTPUT"
+done
+
+# Filter for location ID
+echo "Start filter data for location ID"
+
+LOCATIONS=(
+	5735
+	9590
+	2162162
+)
+
+for loc in "${LOCATIONS[@]}"; do 
+	echo "Filter for location $loc"
+	OUTPUT="data/$loc.csv"
+	head -n 1 "$MERGED_DATA" > "$OUTPUT"
+	grep -e "$loc" "$MERGED_DATA" >> "$OUTPUT"
+	echo "Output file: $OUTPUT"
 done
