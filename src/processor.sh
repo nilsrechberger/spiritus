1eeda7401a208af342e069ee49cec2feda6f5775
Nils Rechberger <nilsrechberger@outlook.com>
2025-11-25 16:13:28 +0100

    feat: iterate over parameter list

--- src/processor.sh
+++ src/processor.sh
@@ -4,7 +4,19 @@
 
 MERGED_DATA="data/merged_data.csv"
 
-# Filter columns for pm10 data
-PM10_DATA="data/pm10.csv"
-head -n 1 "$MERGED_DATA" > "$PM10_DATA"
-grep -e "pm10" $MERGED_DATA >> "$PM10_DATA"
+# Filter columns for parameter
+
+PARAMETERS=(
+	co
+	no2
+	o3
+	pm10
+	pm25
+	so2	
+)
+
+for param in "${PARAMETERS[@]}"; do
+	OUTPUT="data/$param.csv"
+	head -n 1 "$MERGED_DATA" > "$OUTPUT"
+	grep -e "$param" $MERGED_DATA >> "$OUTPUT"
+done
