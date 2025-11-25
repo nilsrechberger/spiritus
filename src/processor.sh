c086baa7fc0f6f21335d6581eebf026a3cec492c
Nils Rechberger <nilsrechberger@outlook.com>
2025-11-25 15:59:01 +0100

    fix: add header to pm10 data output

--- src/processor.sh
+++ src/processor.sh
@@ -5,4 +5,6 @@
 MERGED_DATA="data/merged_data.csv"
 
 # Filter columns for pm10 data
-grep -e "pm10" $MERGED_DATA  > "data/pm10.csv"
+PM10_DATA="data/pm10.csv"
+head -n 1 "$MERGED_DATA" > "$PM10_DATA"
+grep -e "pm10" $MERGED_DATA >> "$PM10_DATA"
