f0093aa1405d74f39754c281fb363bde6a824054
Nils Rechberger <nilsrechberger@outlook.com>
2025-12-03 15:12:36 +0100

    feat: add AQI category

--- src/processor.sh
+++ src/processor.sh
@@ -1,47 +1,105 @@
 #!/bin/bash
 
-# Read data
-
 MERGED_DATA="data/merged_data.csv"
+AQI_DATA="data/AQI_data.csv"
 
-# Filter data for parameter
-echo "Start filter data for paramters"
-PARAMETERS=(
-	co
-	no2
-	o3
-	pm10
-	pm25
-	so2	
-)
+echo "Calculate AQI in $MERGED_DATA according to EEA standards..."
 
-for param in "${PARAMETERS[@]}"; do
-	echo "Filter for $param"
-	OUTPUT="data/$param.csv"
-	head -n 1 "$MERGED_DATA" > "$OUTPUT"
-	grep -e "$param" "$MERGED_DATA" >> "$OUTPUT"
-	echo "Output file: $OUTPUT"
-done
+awk -F, 'BEGIN {OFS=","}
+{
+    # Remove line brake (by windows sys)
+    sub(/\r$/, "", $0)
+}
 
-# Filter for location ID
-echo "Start filter data for location ID"
+# Add new column
+NR==1 { 
+    print $0, "AQI_Category" 
+    next 
+} 
 
-LOCATIONS=(
-	2162162
-	5735
-	9590
-	9589
-	9591
-	9587
-	3732
-	2162150
-	9578
-)
+# Select param and value column
+{
+    raw_param = $7
+    raw_val = $9
+    
+    # Normalize data
+    param = raw_param
+    val = raw_val
+    gsub(/"/, "", param)
+    gsub(/^ +| +$/, "", param)
+    param = tolower(param)
+    
+    gsub(/"/, "", val)
+    gsub(/^ +| +$/, "", val)
 
-for loc in "${LOCATIONS[@]}"; do 
-	echo "Filter for location $loc"
-	OUTPUT="data/$loc.csv"
-	head -n 1 "$MERGED_DATA" > "$OUTPUT"
-	grep -e "$loc" "$MERGED_DATA" >> "$OUTPUT"
-	echo "Output file: $OUTPUT"
-done
+    # Handling missing values
+    val = val + 0
+    
+    cat = "Not defined"
+
+    # Define AQI
+    if (param == "pm25") {
+        if (val <= 10)      cat = "Good"
+        else if (val <= 20) cat = "Fair"
+        else if (val <= 25) cat = "Moderate"
+        else if (val <= 50) cat = "Poor"
+        else if (val <= 75) cat = "Very Poor"
+        else                cat = "Extremely Poor"
+    }
+
+    else if (param == "pm10") {
+        if (val <= 20)       cat = "Good"
+        else if (val <= 35)  cat = "Fair"
+        else if (val <= 50)  cat = "Moderate"
+        else if (val <= 100) cat = "Poor"
+        else if (val <= 200) cat = "Very Poor"
+        else                 cat = "Extremely Poor"
+    }
+
+    else if (param == "no2") {
+        if (val <= 40)       cat = "Good"
+        else if (val <= 90)  cat = "Fair"
+        else if (val <= 120) cat = "Moderate"
+        else if (val <= 230) cat = "Poor"
+        else if (val <= 340) cat = "Very Poor"
+        else                 cat = "Extremely Poor"
+    }
+
+    else if (param == "o3") {
+        if (val <= 50)       cat = "Good"
+        else if (val <= 100) cat = "Fair"
+        else if (val <= 130) cat = "Moderate"
+        else if (val <= 240) cat = "Poor"
+        else if (val <= 380) cat = "Very Poor"
+        else                 cat = "Extremely Poor"
+    }
+
+    else if (param == "so2") {
+        if (val <= 100)      cat = "Good"
+        else if (val <= 200) cat = "Fair"
+        else if (val <= 350) cat = "Moderate"
+        else if (val <= 500) cat = "Poor"
+        else if (val <= 750) cat = "Very Poor"
+        else                 cat = "Extremely Poor"
+    }
+
+    else if (param == "co") {
+        if (val <= 5)       cat = "Good"
+        else if (val <= 7)  cat = "Fair"
+        else if (val <= 10) cat = "Moderate"
+        else if (val <= 15) cat = "Poor"
+        else if (val <= 20) cat = "Very Poor"
+        else                cat = "Extremely Poor"
+    }
+
+    # Falls der Parameter unbekannt ist, zeigen wir ihn im Fehler an
+    else {
+        cat = "No AQI according to EEA"
+    }
+
+    # Add aqi category
+    print $0, cat
+
+}' "$MERGED_DATA" > "$AQI_DATA"
+
+echo "Done! Results in $AQI_DATA"
